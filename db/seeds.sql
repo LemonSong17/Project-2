cf2ae5300ecd4067b76af060c0519291f885b985
George Mendoza <george.a.mendoza@gmail.com>
2019-10-02 22:50:13 -0500

    fixed map and double footer

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-Select * FROM shopping_list;
-INSERT INTO shopping_list(item, category, preferred_amount, current_amount)
-VALUES ('Tooth Paste', 'Hygene', 2, 1), ('Cereal', 'Food', 1, 1), ('Bread', 'Food', 1,1)
